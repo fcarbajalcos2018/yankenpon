ce91004beff84d0dfcfa0d80977ca3454c3ca38d
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2024-02-01 15:10:03 -0500

    Implemented a main function for best practice. Updated the getComputerChoice function to convert randomly generated numeric choices into word based choices. Implemented the getPlayerChoice function.

--- game.js
+++ game.js
@@ -1,4 +1,34 @@
 
-const getComputerChoice = () => Math.round(Math.random() * 2) + 1; // Between 1 and 3 inclusive: 2 + 1 = 3
+const getComputerChoice = () => {
+    const numericChoice = Math.round(Math.random() * 2) + 1; // Between 1 and 3 inclusive: 2 + 1 = 3
+    let choice = '';
+    switch(numericChoice) {
+        case 1:
+            choice = 'rock';
+            break;
+        case 2:
+            choice = "paper";
+            break;
+        case 3:
+            choice = "scissors";
+            break
+        default:
+            choice = 'no choice';
+            break;
+    }
+    return choice;
+}
 
-console.log(getComputerChoice());
+const getPlayerChoice = () => {
+    const choice = prompt('Enter your choice: Rock, Paper or Scissors');
+    const caseCorrectChoice = choice.toLowerCase();
+    console.log(caseCorrectChoice);
+    return 0;
+}
+
+const main = () => {
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    console.log(computerChoice);
+}
+main();
