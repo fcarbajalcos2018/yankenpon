14bd4ab201ffae232b256e7333b3ed9c83536724
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2024-02-01 15:33:56 -0500

    Updated getPlayerChoice to validate string. Added comments to all currently implemented functions.

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 
 const getComputerChoice = () => {
     const numericChoice = Math.round(Math.random() * 2) + 1; // Between 1 and 3 inclusive: 2 + 1 = 3
-    let choice = '';
+    let choice = ''; // Initialise choice as empty string
+    // The following will assign a string containing the respective term from a number from 1 to 3
     switch(numericChoice) {
         case 1:
             choice = 'rock';
@@ -14,16 +15,26 @@ const getComputerChoice = () => {
             break
         default:
             choice = 'no choice';
-            break;
+            break; // Default for debugging purposes
     }
     return choice;
 }
 
 const getPlayerChoice = () => {
+    // Enable user to enter a text parametre
     const choice = prompt('Enter your choice: Rock, Paper or Scissors');
+    // Convert any uppercase letter to lowercase for case-insensitiveness
     const caseCorrectChoice = choice.toLowerCase();
     console.log(caseCorrectChoice);
-    return 0;
+    let response = ''; // Initialise response as empty string
+    if (['rock', 'paper', 'scissors'].includes(caseCorrectChoice)) {
+        response = caseCorrectChoice; // If choice is valid, set response to choice
+    }
+    else {
+        console.warn('You have entered an invalid choice.')
+        response = getPlayerChoice(); // If choice is not valid, recursively call function until valid
+    }
+    return response;
 }
 
 const main = () => {
